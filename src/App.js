75447f765fb489483b21f608e01020480166c06d
Nicolas Hervy <nicolashervy@mbAir.local>
2019-06-12 21:09:12 +0200

    Add styled components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: red;
+  border-radius: 2px;
+  color: white;
+`;
+const H1 = styled.h1`
+  color: orangered;
+  mark {
+    font-size: 0.5em;
+    padding: 0 0.3em;
+  }
+`;
 
 function App() {
   const counter = useSelector(state => state.counter);
@@ -8,13 +22,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Counter: {counter}</h1>
+        <H1>
+          Counter: {counter < 0 ? counter * -1 : counter}{" "}
+          <mark>from redux</mark>
+        </H1>
         <button onClick={() => dispatch({ type: "INCREMENT" })}>
           INCREMENT
         </button>
-        <button onClick={() => dispatch({ type: "DECREMENT" })}>
+        <Button onClick={() => dispatch({ type: "DECREMENT" })}>
           DECREMENT
-        </button>
+        </Button>
       </header>
     </div>
   );
